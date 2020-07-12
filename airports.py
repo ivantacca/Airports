824f0833a1e6b0ef832994bcb207ed7ed3617143
ivantacca <taccadoliivan@gmail.com>
2020-07-12 18:47:31 +0200

    added graph

--- airports.py
+++ airports.py
@@ -1,15 +1,17 @@
+import sys
+
 aps = [
     ['LAX','NYC'],
     ['LAX','TOR'], 
     ['LAX','PAR'],
     ['LAX','LON'],
+    ['LAX','SFO'],
     ['SFO','TOR'],
     ['TOR','NYC'],
     ['NYC','LON'],
     ['LON','FRA'],
     ['FRA','MPX'],
     ['FRA','LAX'],
-    ['FRA','SFO'],
     ['FRA','MOS'],
     ['FRA','AMS'],
     ['MPX','BCN'],
@@ -17,6 +19,7 @@ aps = [
     ['MPX','MAD'],
     ['AMS','BCN'],
     ['AMS','MPX'],
+    ['PAR','FRA']
 ]
 
 class Node:
@@ -37,28 +40,33 @@ class bfs:
                 self.FRONTIER.add(Node(i[1],node))
     def best_path(self):
         self.explore(Node(self.src,None))
+        self.EXPLORED.add(self.src)
         while True:
             if len(self.FRONTIER) == 0:
-                return []
+                return None
             n = self.FRONTIER.pop()
             if n.source not in self.EXPLORED:
                 self.EXPLORED.add(n.source)
+                # if the current node is the target
                 if n.source == self.tg:
-                    path = []
+                    p = []
                     while not (n.parent == None):
-                        path +=[n.source]
+                        p +=[n.source]
                         n = n.parent
-                    path +=[n.source]
-                    return path
-                self.explore(n)
-        
-        return ['LAX','SFO','NYC']
+                    p +=[n.source]
+                    return p
+                else:
+                    self.explore(n)
 
 
-bfs = bfs(aps,'SFO','AMS')
+bfs = bfs(aps,sys.argv[1],sys.argv[2])
+print(f"Flights from {sys.argv[1]} to {sys.argv[2]}")
+
 best_path = bfs.best_path()
-
-if not best_path == None: 
-    print('found a path')
-    print(best_path[::-1])
-    
+for i,a in enumerate(best_path[::-1]):
+    print (a, end='')
+    try:
+        if(best_path[i+1]):
+            print(" -> ", end='')
+    except:
+        print("")
